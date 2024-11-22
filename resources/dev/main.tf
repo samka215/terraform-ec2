e08210964a2399286e8b82e2778545a3d5928d67
devopseasylearning <info@devopseasylearning.com>
2024-11-22 16:22:12 -0600

    add user data update

--- resources/dev/main.tf
+++ resources/dev/main.tf
@@ -3,7 +3,39 @@ locals {
   instance_type               = "t2.micro"
   key_name                    = "s7-key"
   availability_zone           = "us-east-1a"
-  user_data                   = file("${path.module}/script/install_jenkins.sh")
+  user_data                   = <<-EOT
+#!/bin/bash
+
+# https://www.jenkins.io/doc/book/installing/linux/#debianubuntu
+# Please install the Long Term Support release
+
+function jenkins_installation {
+    sudo apt update
+    ## Set vim as default text editor
+    sudo update-alternatives --set editor /usr/bin/vim.basic
+    sudo update-alternatives --set vi /usr/bin/vim.basic
+    sudo apt install fontconfig openjdk-17-jre -y
+    java -version
+
+    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install jenkins -y
+
+    sudo systemctl start jenkins
+    sudo systemctl enable jenkins
+
+    INSTANCE_PUBLIC_IP=$(curl -s ifconfig.me)
+    ADMIN_KEY=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
+    JENKINS_URL="http://$INSTANCE_PUBLIC_IP:8080"
+    echo "Jenkins is installed and running. You can access it at $JENKINS_URL"
+    echo "This is the initialAdminPassword: $ADMIN_KEY"
+}
+jenkins_installation
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
+  EOT
   tenancy                     = "default"
   associate_public_ip_address = true
   volume_type                 = "gp3"
@@ -36,6 +68,13 @@ module "ec2_instance" {
   iops                        = local.iops
   delete_on_termination       = local.delete_on_termination
   encrypted                   = local.encrypted
-  tags = local.tags
+  tags                        = local.tags
+}
 
+output "public_ip" {
+  value = module.ec2_instance.public_ip
+}
+
+output "private_ip" {
+  value = module.ec2_instance.private_ip
 }
